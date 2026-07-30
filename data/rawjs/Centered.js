deed7de13420d79a6d1e1d28fd8ebb37c24c19a0
GimloaderBot <gimloaderbot@gmail.com>
2026-07-30 21:30:40 +0200

    Backfill raw js (2026/4/13)

--- data/rawjs/Centered.js
+++ data/rawjs/Centered.js
@@ -1,1 +1,1 @@
-import{d as e}from"./_index.js";const t=e.div.attrs({className:"flex-center"})``,a=e.div.attrs({className:"flex vc"})``,c=e(t).attrs({className:"flex-column"})``,r=e.div.attrs({className:"flex vc"})``;export{t as C,a as V,c as a,r as b};
+import{b as e}from"./_index.js";const t=e.div.attrs({className:"flex-center"})``,a=e.div.attrs({className:"flex vc"})``,c=e(t).attrs({className:"flex-column"})``,r=e.div.attrs({className:"flex vc"})``;export{t as C,a as V,c as a,r as b};
