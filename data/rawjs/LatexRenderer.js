642f8e0b05f6f1082f71b25cea7670130dda70bd
GimloaderBot <gimloaderbot@gmail.com>
2026-07-30 21:31:44 +0200

    Backfill raw js (2026/4/30)

--- data/rawjs/LatexRenderer.js
+++ data/rawjs/LatexRenderer.js
@@ -1,4 +1,4 @@
-import{j as e,r,ay as s,d as a}from"./_index.js";import{q as o}from"./mathlive.min.js";const l=t=>{const n=r.useRef(null);return r.useEffect(()=>{o(n.current,{})},[]),e.jsxs(e.Fragment,{children:[e.jsx(c,{}),e.jsx(x,{children:e.jsx("math-field",{className:"latex",tabIndex:"-1",ref:n,"read-only":!0,style:{display:"flex",justifyContent:"center",alignItems:"center"},children:t.latex})})]})},f=t=>e.jsx(l,{...t},t.latex),x=a.div`
+import{j as e,r,ay as s,b as a}from"./_index.js";import{q as o}from"./mathlive.min.js";const l=t=>{const n=r.useRef(null);return r.useEffect(()=>{o(n.current,{})},[]),e.jsxs(e.Fragment,{children:[e.jsx(c,{}),e.jsx(x,{children:e.jsx("math-field",{className:"latex",tabIndex:"-1",ref:n,"read-only":!0,style:{display:"flex",justifyContent:"center",alignItems:"center"},children:t.latex})})]})},d=t=>e.jsx(l,{...t},t.latex),x=a.div`
   pointer-events: none;
 `,c=s`
   .latex {
@@ -6,4 +6,4 @@ import{j as e,r,ay as s,d as a}from"./_index.js";import{q as o}from"./mathlive.m
       cursor: inherit !important;
     }
   }
-`;export{f as default};
+`;export{d as default};
--- data/rawjs/ReactionToMultipleProps.js
+++ data/rawjs/ReactionToMultipleProps.js
@@ -1,1 +1,1 @@
-import{dO as n}from"./_index.js";const p=(r,t,a)=>{const c=[];return r.forEach(o=>{const s=n(o,t,a);c.push(s)}),()=>{c.forEach(o=>{o()})}};export{p as R};
+import{dP as n}from"./_index.js";const p=(r,t,a)=>{const c=[];return r.forEach(o=>{const s=n(o,t,a);c.push(s)}),()=>{c.forEach(o=>{o()})}};export{p as R};
