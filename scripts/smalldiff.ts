54adf84d69381021fdbfe153da4a618797bf27e1
TheLazySquid <thelazysquid2@gmail.com>
2026-09-08 18:46:41 -0400

    Make smalldiff script suck less

--- scripts/smalldiff.ts
+++ scripts/smalldiff.ts
@@ -1,25 +1,67 @@
-import { readFile, writeFile } from "node:fs/promises";
+import { spawn, execSync } from "node:child_process";
+import { writeFile, rm, mkdir } from "node:fs/promises";
+import * as acorn from "acorn";
+import * as walk from "acorn-walk";
+import { generate } from "escodegen";
 
-const diff = await readFile("./scripts/change.diff", "utf-8");
-const files = diff.split(/(^|\n)diff --git /);
-
-files.splice(0, 1);
-
-for(let i = files.length - 1; i >= 0; i--) {
-    const parts = files[i].split("\n@@");
-    
-    for(let j = parts.length - 1; j >= 1; j--) {
-        const added = parts[j].split("\n+").length;
-        const removed = parts[j].split("\n-").length;
-
-        if(added === removed) parts.splice(j, 1);
-    }
-
-    if(parts.length === 1) {
-        files.splice(i, 1);
-    } else {
-        files[i] = "diff --git " + parts.join("\n@@");
-    }
+const [oldCommit, newCommit, file] = process.argv.slice(2);
+if(!oldCommit || !newCommit || !file) {
+    throw new Error("Usage: node scripts/smalldiff.ts <oldCommit> <newCommit> <file>");
 }
 
-await writeFile("./scripts/small.diff", files.join("\n"));
+await rm("tmp", { recursive: true, force: true });
+await mkdir("tmp", { recursive: true });
+
+const oldPath = await normalize(oldCommit, file);
+const newPath = await normalize(newCommit, file);
+
+try {
+    execSync(`git diff --no-index ${oldPath} ${newPath} --output=tmp/changes.diff`);
+} catch {}
+
+async function normalize(commit: string, file: string) {
+    const code = await getFile(commit, `data/js/${file}`);
+    const ast = acorn.parse(code, { ecmaVersion: "latest", sourceType: "module" });
+    
+    walk.simple(ast, {
+        Pattern(node) {
+			if(node.type !== "Identifier") return;
+            node.name = "a";
+		},
+        Identifier(node) {
+            node.name = "a";
+        },
+        Program(node) {
+            node.body = node.body.filter((n) => n.type !== "ImportDeclaration" && n.type !== "ExportNamedDeclaration");
+        },
+        ArrayExpression(node) {
+            if(!node.elements.every((e) => e.type === "Literal")) return;
+
+            if(node.elements.every((e) => typeof e.value === "number")) {
+                node.elements = [];
+            } else if(node.elements.every((e) => typeof e.value === "string" && e.value.startsWith("assets"))) {
+                node.elements = [];
+            }
+        }
+    });
+
+    const path = `tmp/normalized-${commit.slice(0, 5)}.js`;
+    await writeFile(path, generate(ast));
+
+    return path;
+}
+
+function getFile(commit: string, file: string): Promise<string> {
+    return new Promise<string>((res) => {
+        const child = spawn("git", ["show", `${commit}:${file}`]);
+        let buffer = "";
+
+        child.stdout.on("data", (data) => {
+            buffer += data.toString();
+        });
+
+        child.on("close", () => {
+            res(buffer);
+        });
+    });
+}
