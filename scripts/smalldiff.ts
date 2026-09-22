f09d7ab9f8fdb4f988a49c296c89a939abdab2d4
retrozy1 <202344625+retrozy1@users.noreply.github.com>
2026-09-22 13:29:14 -0400

    Make smalldiff script compare entire directory

--- scripts/smalldiff.ts
+++ scripts/smalldiff.ts
@@ -4,30 +4,47 @@ import * as acorn from "acorn";
 import * as walk from "acorn-walk";
 import { generate } from "escodegen";
 
-const [oldCommit, newCommit, file] = process.argv.slice(2);
-if(!oldCommit || !newCommit || !file) {
-    throw new Error("Usage: node scripts/smalldiff.ts <oldCommit> <newCommit> <file>");
+const [oldCommit, newCommit] = process.argv.slice(2);
+if(!oldCommit || !newCommit) {
+    throw new Error("Usage: node scripts/smalldiff.ts <oldCommit> <newCommit>");
 }
 
 await rm("tmp", { recursive: true, force: true });
 await mkdir("tmp", { recursive: true });
 
-const oldPath = await normalize(oldCommit, file);
-const newPath = await normalize(newCommit, file);
+const oldPath = await normalizeDir(oldCommit);
+const newPath = await normalizeDir(newCommit);
 
 try {
     execSync(`git diff --no-index ${oldPath} ${newPath} --output=tmp/changes.diff`);
 } catch {}
 
-async function normalize(commit: string, file: string) {
+async function normalizeDir(commit: string) {
+  const path = `tmp/normalized-${commit.slice(0, 5)}`;
+  await mkdir(path);
+  const files = execSync(`git show --name-only ${commit}:data/js/`).toString().split('\n').slice(2);
+  for (const file of files) {
+    if (!file) continue;
     const code = await getFile(commit, `data/js/${file}`);
+    try {
+      const normalized = await normalizeCode(code);
+      await writeFile(`${path}/${file}`, normalized);
+    } catch(err) {
+      throw new Error(`Error at ${file}: ${err}`)
+    }
+  }
+
+  return path;
+}
+
+async function normalizeCode(code: string) {
     const ast = acorn.parse(code, { ecmaVersion: "latest", sourceType: "module" });
     
     walk.simple(ast, {
         Pattern(node) {
-			if(node.type !== "Identifier") return;
+        if(node.type !== "Identifier") return;
             node.name = "a";
-		},
+        },
         Identifier(node) {
             node.name = "a";
         },
@@ -35,20 +52,27 @@ async function normalize(commit: string, file: string) {
             node.body = node.body.filter((n) => n.type !== "ImportDeclaration" && n.type !== "ExportNamedDeclaration");
         },
         ArrayExpression(node) {
+            if(node.elements.some(e => !e)) return;
             if(!node.elements.every((e) => e.type === "Literal")) return;
-
             if(node.elements.every((e) => typeof e.value === "number")) {
                 node.elements = [];
             } else if(node.elements.every((e) => typeof e.value === "string" && e.value.startsWith("assets"))) {
                 node.elements = [];
             }
+        },
+        CallExpression(node) {
+            if(
+                node.callee.type === "MemberExpression" &&
+                !node.callee.computed &&
+                node.callee.property.type === "Identifier" &&
+                (node.callee.property.name === "lazyWithPreload" || node.callee.property.name === "lazy")
+            ) {
+                node.arguments = [];
+            }
         }
     });
 
-    const path = `tmp/normalized-${commit.slice(0, 5)}.js`;
-    await writeFile(path, generate(ast));
-
-    return path;
+    return generate(ast);
 }
 
 function getFile(commit: string, file: string): Promise<string> {
